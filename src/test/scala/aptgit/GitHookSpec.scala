7ad229b827aed59f058e9367e7081c7b7ada815f
ScalaWilliam <william@scalawilliam.com>
2018-03-10 12:10:25 +0800

    Start up a Git server and verify we can log-in into it.

--- src/test/scala/aptgit/GitHookSpec.scala
+++ src/test/scala/aptgit/GitHookSpec.scala
@@ -3,23 +3,50 @@ import java.nio.file.{Files, Paths}
 
 import com.whisk.docker.impl.spotify.DockerKitSpotify
 import com.whisk.docker.scalatest.DockerTestKit
-import com.whisk.docker.{DockerContainer, DockerReadyChecker}
+import com.whisk.docker.{DockerContainer, VolumeMapping}
+import org.scalatest.Matchers._
 import org.scalatest._
 
 class GitHookSpec extends FreeSpec with DockerTestKit with DockerKitSpotify {
 
-  private val DefaultMongodbPort = 27017
+  private val StandardSshPort = 22
+  private val MappedSshPort = 2222
 
-  private val mongodbContainer = DockerContainer("mongo:3.0.6")
-    .withPorts(DefaultMongodbPort -> None)
-    .withReadyChecker(
-      DockerReadyChecker.LogLineContains("waiting for connections on port"))
-    .withCommand("mongod", "--nojournal", "--smallfiles", "--syncdelay", "0")
+  private val keysMapping = VolumeMapping(
+    host = Paths.get("pub-sample").toAbsolutePath.toString,
+    container = "/git-server/keys",
+    rw = false,
+  )
+
+  private val reposMapping = VolumeMapping(
+    host = Paths.get("repos").toAbsolutePath.toString,
+    container = "/git-server/repos",
+    rw = true,
+  )
+
+  private val gitServerContainer = DockerContainer("jkarlos/git-server-docker")
+    .withPorts(StandardSshPort -> Some(MappedSshPort))
+    .withVolumes(keysMapping :: reposMapping :: Nil)
 
   override def dockerContainers: List[DockerContainer] =
-    mongodbContainer :: super.dockerContainers
+    gitServerContainer :: super.dockerContainers
 
   def pushToGitServer(): Unit = {}
+
+  "Log-in is successful" in {
+    val cmd = Seq("ssh",
+                  "-tt",
+                  "git@127.0.0.1",
+                  "-p",
+                  s"${MappedSshPort}",
+                  "-o",
+                  "StrictHostKeyChecking=no",
+                  "-o",
+                  "UserKnownHostsFile=/dev/null")
+    import scala.sys.process._
+    val result = cmd.lineStream_!
+    exactly(1, result) should include("Welcome to git-server-docker")
+  }
 
   "It works" in {
     val expectedFile = Paths.get("received")
