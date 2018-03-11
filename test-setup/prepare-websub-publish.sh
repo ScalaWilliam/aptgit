6b7c0890e55bb177b499fd06305a83cf7ae0a9ef
ScalaWilliam <william@scalawilliam.com>
2018-03-11 09:06:51 +0800

    Attempt to fix #4.

--- test-setup/prepare-websub-publish.sh
+++ test-setup/prepare-websub-publish.sh
@@ -2,5 +2,7 @@
 echo GIT_DIR="/git-server/repos/sample.git" python /test-setup/publish.py >> /git-server/repos/sample.git/hooks/post-receive
 cd /git-server/repos/sample.git/
 cp /test-setup/default.html /target/blah.html
+chown git:git /target
+chown git:git /target/blah.html
 git config --add websub.files /target/blah.html
 GIT_DIR=/git-server/repos/sample.git python /test-setup/set-self-url.py http://simple_http_server:8080/blah.html
